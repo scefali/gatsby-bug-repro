2cb6de4161bb0cb493b7f651a7749e78eee0340f
Stephen Cefali <scefali@sentry.io>
2022-05-13 11:32:19 -0700

    add initial code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,499 @@
-import * as React from "react"
+import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import Layout from "../layouts"
+import { rhythm } from "../utils/typography"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+class Index extends React.Component {
+  render() {
+    const data = this.props.data
+    const images = data.allImageSharp.edges
+    const fluid = data.fluidImages.childImageSharp.fluid
+    const fixed = data.fixedImages.childImageSharp.fixed
+    const cropDefault = data.cropDefault.childImageSharp.resize
+    const cropBottomLeft = data.cropBottomLeft.childImageSharp.resize
+    const cropEntropy = data.cropEntropy.childImageSharp.resize
+    const cropCenter = data.cropCenter.childImageSharp.resize
+    const fluidDuotoneOriginal = data.fluidDuotoneOriginal.childImageSharp.fluid
+    const fluidDuotone25 = data.fluidDuotone25.childImageSharp.fluid
+    const fluidDuotone50 = data.fluidDuotone50.childImageSharp.fluid
+    const fluidDuotone75 = data.fluidDuotone75.childImageSharp.fluid
+    const fluidDuotone = data.fluidDuotone.childImageSharp.fluid
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-// markup
-const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
+    return (
+      <Layout>
+        <p>
+          <a href="https://www.gatsbyjs.com/plugins/gatsby-transformer-sharp/">
+            <code>gatsby-transformer-sharp</code>
           </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
+          {` `}
+          exposes several image processing GraphQL functions built on the
+          {` `}
+          <a href="https://github.com/lovell/sharp">
+            Sharp image processing library
+          </a>
+          . With it and
+          {` `}
+          <a href="https://www.gatsbyjs.com/plugins/gatsby-image/">
+            Gatsby Image
+          </a>
+          {` `}
+          you can easily add fast, optimized, responsive images to your site.
+        </p>
+        <p>
+          <strong>
+            Consult the
+            {` `}
+            <a href="https://www.gatsbyjs.com/plugins/gatsby-plugin-sharp/">
+              documentation
+            </a>
+            {` `}
+            or peep the
+            {` `}
+            <a href="https://github.com/gatsbyjs/gatsby/tree/master/examples/image-processing">
+              code
+            </a>
+            {` `}
+            of this example site for more information.
+          </strong>
+        </p>
+
+        <h2
+          style={{
+            clear: `left`,
+            paddingTop: rhythm(2),
+          }}
+        >
+          <a href="https://www.gatsbyjs.com/plugins/gatsby-plugin-sharp/#resize">
+            <code>
+              <strong>resize</strong>
+            </code>
+          </a>
+        </h2>
+
+        <p>
+          Easily resize and intelligently crop images given a{` `}
+          <code>width</code>
+          {` `}
+          and optionally a <code>height</code>.
+        </p>
+
+        <p>
+          The
+          {` `}
+          <code>rotate</code> option
+          {` `}
+          exposes Sharp
+          {`'`}s{` `}
+          <a href="http://sharp.pixelplumbing.com/en/stable/api-operation/#rotate">
+            <code>rotate</code>
+          </a>
+          {` `}.
+        </p>
+
+        <h3>
+          <code>
+            <small>resize(width: 125, height: 125, rotate: 180)</small>
+          </code>
+        </h3>
+
+        <ul style={{ ...styles.ul, ...styles.row }}>
+          {images.map(image => (
+            <li style={styles.column20} key={image.node.resize.src}>
+              <img
+                src={image.node.resize.src}
+                alt={image.node.resize.originalName}
+              />
+            </li>
+          ))}
+        </ul>
+        <p
+          style={{
+            clear: `left`,
+          }}
+        >
+          We also expose all of Sharp
+          {`'`}s{` `}
+          <a href="http://sharp.pixelplumbing.com/en/stable/api-resize/#crop">
+            <code>crop</code>
+          </a>
+          {` `}
+          options with <code>cropFocus</code>.
+        </p>
+        <p>
+          The default is
+          <code>ATTENTION</code>, which the Sharp documentation describes as
+          "focus on the region with the highest luminance frequency, colour
+          saturation and presence of skin tones" – which is why we actually see
+          F. Scott Fitzgerald centered in the first thumbnail:
+        </p>
+
+        <ul style={{ ...styles.ul, ...styles.row }}>
+          <li style={styles.column25}>
+            <img
+              src={cropDefault.src}
+              alt={`File ${cropDefault.originalName} with a default crop`}
+            />
           </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
+          <li style={styles.column25}>
+            <img
+              src={cropBottomLeft.src}
+              alt={`File ${cropBottomLeft.originalName} cropped to the bottom left`}
+            />
+            <p>
+              <small>
+                <code>cropFocus: SOUTHWEST</code>
+              </small>
+            </p>
+          </li>
+          <li style={styles.column25}>
+            <img
+              src={cropEntropy.src}
+              alt={`File ${cropEntropy.originalName} with an "entropy" crop`}
+            />
+            <p>
+              <small>
+                <code>cropFocus: ENTROPY</code>
+              </small>
+            </p>
+          </li>
+          <li style={styles.column25}>
+            <img
+              src={cropCenter.src}
+              alt={`File ${cropCenter.originalName} cropped to the centre`}
+            />
+            <p>
+              <small>
+                <code>cropFocus: CENTER</code>
+              </small>
+            </p>
+          </li>
+        </ul>
+
+        <h2
+          style={{
+            clear: `left`,
+            paddingTop: rhythm(2),
+          }}
+        >
+          <a href="https://www.gatsbyjs.com/plugins/gatsby-plugin-sharp/#fluid">
+            <code>
+              <strong>fluid</strong>
+            </code>
+          </a>
+        </h2>
+        <p>
+          For when you want an image that stretches across a fluid width
+          container but will download the smallest image needed for the device
+          e.g. a smartphone will download a much smaller image than a desktop
+          device.
+        </p>
+        <p>
+          If the max width of the container for the rendered markdown file is
+          800px, the fluid sizes would then be: 200, 400, 800, 1200, 1600, 2400
+          – enough to provide close to the optimal image size for every device
+          size / screen resolution.
+        </p>
+        <p>
+          On top of that, <code>fluid</code>
+          {` `}
+          returns everything else (namely
+          {` `}
+          <code>aspectRatio</code>
+          {` `}
+          and a{` `}
+          <code>base64</code>
+          {` `}
+          image to use as a placeholder) you need to implement the {`"blur up"`}
+          technique popularized by
+          {` `}
+          <a href="https://jmperezperez.com/medium-image-progressive-loading-placeholder/">
+            Medium
+          </a>
+          {` `}
+          and
+          {` `}
+          <a href="https://code.facebook.com/posts/991252547593574/the-technology-behind-preview-photos/">
+            Facebook
+          </a>
+          {` `}
+          (and also available as a Gatsby plugin for Markdown content as
+          {` `}
+          <a href="https://www.gatsbyjs.com/plugins/gatsby-remark-images/">
+            gatsby-remark-images
+          </a>
+          ).
+        </p>
+        <p>
+          The <code>duotone</code> option (see
+          {` `}
+          <a href="https://alistapart.com/article/finessing-fecolormatrix">I</a>
+          ,{` `}
+          <a href="http://blog.72lions.com/blog/2015/7/7/duotone-in-js">II</a>,
+          {` `}
+          <a href="https://ines.io/blog/dynamic-duotone-svg-jade">III</a>
+          ), given two hex colors
+          {` `}
+          <code>shadow</code>
+          {` `}
+          and <code>highlight</code> defining start and end color of the duotone
+          gradient, converts the source image colors to match a gradient color
+          chosen based on each pixels relative luminance.
+        </p>
+
+        <p>
+          The <code>toFormat</code> option lets you convert the source image to
+          another image format. We use {`"PNG"`} here to ensure that the
+          duotoned image does not show any JPG artifacts.
+        </p>
+
+        <h3>
+          <small>
+            fluid(duotone:
+            {` `}
+            {`{ `}
+            highlight: {`"#f00e2e"`}, shadow: {`"#192550"`} {`}`}, toFormat:
+            PNG)
+          </small>
+        </h3>
+
+        <Img fluid={fluid} />
+
+        <h3 style={{ marginTop: rhythm(2) }}>
+          <small>
+            fluid(duotone:
+            {` `}
+            {`{ `}
+            highlight: {`"#0ec4f1"`}, shadow: {`"#192550"`}, opacity: 50 {`}`})
+          </small>
+        </h3>
+
+        <div style={styles.row}>
+          <div style={styles.column20}>
+            <Img fluid={fluidDuotoneOriginal} />
+          </div>
+          <div style={styles.column20}>
+            <Img fluid={fluidDuotone25} />
+          </div>
+          <div style={styles.column20}>
+            <Img fluid={fluidDuotone50} />
+          </div>
+          <div style={styles.column20}>
+            <Img fluid={fluidDuotone75} />
+          </div>
+          <div style={styles.column20}>
+            <Img fluid={fluidDuotone} />
+          </div>
+        </div>
+
+        <p>
+          By setting an optional third parameter
+          {` `}
+          <code>opacity</code> for <code>duotone</code>, a semi-transparent
+          version of the duotone
+          {`'`}d image will be composited over the original allowing the
+          original image and its colors to partially {`"`}
+          shine through
+          {`"`}.
+        </p>
+
+        <h2
+          style={{
+            paddingTop: rhythm(2),
+          }}
+        >
+          <a href="https://www.gatsbyjs.com/plugins/gatsby-plugin-sharp/#fixed">
+            <code>fixed</code>
+          </a>
+        </h2>
+        <p>
+          For when you want a fixed sized image but that has different sized
+          thumbnails for screens that support different density of images
+        </p>
+        <p>
+          Automatically create images for different resolutions — we do 1x,
+          1.5x, and 2x.
+          {` `}
+        </p>
+
+        <p>
+          The
+          {` `}
+          <code>grayscale</code> option
+          {` `}
+          uses Sharp
+          {`'`}s{` `}
+          <a href="http://sharp.pixelplumbing.com/en/stable/api-colour/#greyscale">
+            <code>greyscale</code>
+          </a>
+          {` `}
+          to convert the source image to 8-bit greyscale, 256 shades of grey.
+        </p>
+
+        <Img fixed={fixed} />
+      </Layout>
+    )
+  }
 }
 
-export default IndexPage
+const styles = {}
+
+styles.row = {
+  display: `flex`,
+  flexWrap: `wrap`,
+  margin: `8px -4px 1rem`,
+}
+
+styles.ul = {
+  padding: `0`,
+  listStyle: `none`,
+}
+
+styles.column20 = {
+  flexShrink: 0,
+  flexGrow: 0,
+  color: `#999`,
+  width: `20%`,
+  padding: `0 4px`,
+  margin: 0,
+}
+
+styles.column25 = {
+  flexShrink: 0,
+  flexGrow: 0,
+  color: `#999`,
+  width: `25%`,
+  padding: `0 4px`,
+  margin: 0,
+}
+
+export default Index
+
+export const pageQuery = graphql`
+  query {
+    allImageSharp {
+      edges {
+        node {
+          ... on ImageSharp {
+            resize(width: 125, height: 125, rotate: 180) {
+              src
+            }
+          }
+        }
+      }
+    }
+    fluidImages: file(
+      relativePath: { regex: "/fecolormatrix-kanye-west.jpg/" }
+    ) {
+      childImageSharp {
+        fluid(
+          duotone: { highlight: "#f00e2e", shadow: "#192550" }
+          traceSVG: {
+            color: "#f00e2e"
+            turnPolicy: TURNPOLICY_MINORITY
+            blackOnWhite: false
+          }
+          toFormat: PNG
+        ) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    fluidDuotone: file(
+      relativePath: { regex: "/fecolormatrix-kanye-west.jpg/" }
+    ) {
+      childImageSharp {
+        fluid(
+          maxWidth: 120
+          duotone: { highlight: "#0ec4f1", shadow: "#192550" }
+          traceSVG: { color: "#1E2151" }
+        ) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    fluidDuotone50: file(
+      relativePath: { regex: "/fecolormatrix-kanye-west.jpg/" }
+    ) {
+      childImageSharp {
+        fluid(
+          maxWidth: 120
+          duotone: { highlight: "#0ec4f1", shadow: "#192550", opacity: 50 }
+          traceSVG: { color: "#A7DEF6" }
+        ) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    fluidDuotone75: file(
+      relativePath: { regex: "/fecolormatrix-kanye-west.jpg/" }
+    ) {
+      childImageSharp {
+        fluid(
+          maxWidth: 120
+          duotone: { highlight: "#0ec4f1", shadow: "#192550", opacity: 75 }
+          traceSVG: { color: "#0ec4f1" }
+        ) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    fluidDuotone25: file(
+      relativePath: { regex: "/fecolormatrix-kanye-west.jpg/" }
+    ) {
+      childImageSharp {
+        fluid(
+          maxWidth: 120
+          traceSVG: { color: "#D1EFFB" }
+          duotone: { highlight: "#0ec4f1", shadow: "#192550", opacity: 25 }
+        ) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    fluidDuotoneOriginal: file(
+      relativePath: { regex: "/fecolormatrix-kanye-west.jpg/" }
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 120, traceSVG: { color: "#e7f7fe" }, toFormat: PNG) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    fixedImages: file(relativePath: { regex: "/lol.jpg/" }) {
+      childImageSharp {
+        fixed(grayscale: true, width: 500) {
+          ...GatsbyImageSharpFixed_withWebp
+        }
+      }
+    }
+    cropDefault: file(relativePath: { regex: "/f-scott.jpg/" }) {
+      childImageSharp {
+        resize(width: 180, height: 180) {
+          src
+        }
+      }
+    }
+    cropBottomLeft: file(relativePath: { regex: "/f-scott.jpg/" }) {
+      childImageSharp {
+        resize(width: 180, height: 180, cropFocus: SOUTHWEST) {
+          src
+        }
+      }
+    }
+    cropEntropy: file(relativePath: { regex: "/f-scott.jpg/" }) {
+      childImageSharp {
+        resize(width: 180, height: 180, cropFocus: ENTROPY) {
+          src
+        }
+      }
+    }
+    cropCenter: file(relativePath: { regex: "/f-scott.jpg/" }) {
+      childImageSharp {
+        resize(width: 180, height: 180, cropFocus: CENTER) {
+          src
+        }
+      }
+    }
+  }
+`
